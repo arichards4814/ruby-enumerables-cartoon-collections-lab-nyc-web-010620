9110592ea2c2626b96a12642605de39fbe35340e
Andrew Richards <arichards4814@gmail.com>
2019-11-02 23:39:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,10 @@ def summon_captain_planet(arr)
   
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arr)
   # Your code here
+  arr.
+  
 end
 
 def find_the_cheese# code an argument here
