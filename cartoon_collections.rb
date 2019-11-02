4f6aef90cc0d724deb7dd4bd27016ec4bc70cba7
Andrew Richards <arichards4814@gmail.com>
2019-11-02 23:16:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(arr)
   
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)
   # Your code here
+  
+  
 end
 
 def long_planeteer_calls# code an argument here
